e2f962e32f985e4933c14ed9d7529c77ed9d1597
ass2888 <aseelm9988@gmail.com>
2025-11-21 19:56:02 +0300

    Update service-worker.js

--- client/service-worker.js
+++ client/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'codeai-static-v1.15';
+const CACHE_NAME = 'codeai-static-v1.16';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/styles.css', '/app.js'];
 self.addEventListener('install', (e) => {
   e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
