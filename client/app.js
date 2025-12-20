0a252e4e1207dd80895143a05ee2e757a4aface4
ass2888 <aseelm9988@gmail.com>
2025-12-20 19:30:57 +0300

    Update app.js

--- client/app.js
+++ client/app.js
@@ -1897,7 +1897,7 @@ document.getElementById('codeToggleBtn').addEventListener('click', () => {
 });
 
 // --- Version Display Logic ---
-    const APP_VERSION = 'v1.274.3'; // يمكنك تحديث هذا يدوياً عند كل تحديث للكاش
+    const APP_VERSION = 'v1.274.5'; // يمكنك تحديث هذا يدوياً عند كل تحديث للكاش
     const versionEl = document.getElementById('appVersion');
     if(versionEl) versionEl.textContent = APP_VERSION;
     
