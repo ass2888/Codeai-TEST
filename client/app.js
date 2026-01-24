75cecd3e74144cac93b7cf42071a9e1436326140
ass2888 <aseelm9988@gmail.com>
2026-01-24 21:35:02 +0300

    Update app.js

--- client/app.js
+++ client/app.js
@@ -1,4 +1,4 @@
-const APP_VERSION = 'v1.276.6'; // يمكنك تحديث هذا يدوياً عند كل تحديث للكاش
+const APP_VERSION = 'v1.276.7'; // يمكنك تحديث هذا يدوياً عند كل تحديث للكاش
 const versionEl = document.getElementById('appVersion');
 if(versionEl) versionEl.textContent = APP_VERSION;
 
